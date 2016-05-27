c7746d18ed007f4d03322b09fc618e5d04b89093
Pierre de LESPINAY <pierre.delespinay@masao.eu>
2016-05-27 14:17:07 +0200

    Refactor - Alphanumeric chars are allowed and 2 params added

--- jquery-code-scanner.js
+++ jquery-code-scanner.js
@@ -1,34 +1,47 @@
-$.fn.codeScanner = function (options) {
-    var onScan = (typeof options === 'object') &&
-        options['onScan'] ? options['onScan'] :
-        function (element, barcode) {
-            $(element).val(barcode);
-        };
-    var pressed = false;
-    var chars = [];
-    var self = this;
+(function ($) {
+    $.fn.codeScanner = function (options) {
+        var settings = $.extend({}, $.fn.codeScanner.defaults, options);
 
-    $(window).keypress(function(e) {
-        if (e.which >= 48 && e.which <= 57) {
-            chars.push(String.fromCharCode(e.which));
-        }
-        // console.log(e.which + ':' + chars.join('|'));
-        if (pressed == false) {
-            setTimeout(function () {
-                if (chars.length >= 10) {
-                    var barcode = chars.join('');
-                    onScan(self, barcode);
+        return this.each(function () {
+            var pressed = false;
+            var chars = [];
+            $(window).keypress(function (e) {
+                var keycode = (e.which) ? e.which : e.keyCode;
+                if ((keycode >= 65 && keycode <= 90) ||
+                    (keycode >= 97 && keycode <= 122) ||
+                    (keycode >= 48 && keycode <= 57)
+                ) {
+                    chars.push(String.fromCharCode(e.which));
                 }
-                chars = [];
-                pressed = false;
-            }, 500);
-        }
-        pressed = true;
-    });
+                // console.log(e.which + ":" + chars.join("|"));
+                if (pressed == false) {
+                    setTimeout(function () {
+                        if (chars.length >= settings.minEntryChars) {
+                            var barcode = chars.join('');
+                            settings.onScan($(this), barcode);
+                        }
+                        chars = [];
+                        pressed = false;
+                    }, settings.maxEntryTime);
+                }
+                pressed = true;
+            });
 
-    $(this).keypress(function (e) {
-        if (e.which === 13) {
-            e.preventDefault();
+            $(this).keypress(function (e) {
+                if (e.which === 13) {
+                    e.preventDefault();
+                }
+            });
+
+            return $(this);
+        });
+    };
+
+    $.fn.codeScanner.defaults = {
+        minEntryChars: 8,
+        maxEntryTime: 100,
+        onScan: function (element, barcode) {
+            $(element).val(barcode);
         }
-    });
-};
+    };
+})(jQuery);
